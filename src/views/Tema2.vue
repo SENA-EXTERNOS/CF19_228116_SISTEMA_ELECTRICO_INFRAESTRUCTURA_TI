424f069cc8d2d7a99430d87eafbfce7efd7a7c45
Camilo Bolaño <80546125+elrilacho@users.noreply.github.com>
2023-04-12 11:55:18 -0500

    Componente listo

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -159,7 +159,7 @@
 
     figure.mb-5(data-aos="zoom-in")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/DN1XgcanPX4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     .titulo-tercero(data-aos="fade-right")
       h3 Cables eléctricos
